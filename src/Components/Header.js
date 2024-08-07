45da6168e4dc73692e8b446e839eedc12792f16b
KrisHHFI <kristopherpepperuk@gmail.com>
2024-08-07 16:56:02 +0300

    Some work on the navigation page making it relevant to the project

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,27 +1,27 @@
 import React from 'react';
-import CompanyLogo from '../Images/Company-Logo.png';
-import CompanyText from '../Images/Company-Text.png';
+import KPText from '../Images/KP.png';
+import PortfolioText from '../Images/Portfolio.png';
 
 export default function Header({ activeLink, onLinkClick }) {
     
     return (
         <div className="Header-Container">
             <div className="Header-Item-1 Header-Item">
-                <img src={CompanyLogo} alt="Company Logo" className="CompanyLogo" />
+                <img src={KPText} alt="Company Logo" className="KPText" />
             </div>
             <div className="Header-Item hidden">
-                <img src={CompanyText} alt="Company Text" className="CompanyText" />
+                <img src={PortfolioText} alt="Company Text" className="PortfolioText" />
             </div>
             <div className="Header-Item-3 Header-Item">
                 <div className="Nav-Bar">
                     <div className={`Nav-Item ${activeLink === 'Products' ? 'active' : ''}`}>
-                        <a href="#" onClick={() => onLinkClick('Products')}>Products</a>
+                        <a href="#" onClick={() => onLinkClick('Products')}>Coding</a>
                     </div>
                     <div className={`Nav-Item ${activeLink === 'About' ? 'active' : ''}`}>
-                        <a href="#" onClick={() => onLinkClick('About')}>About</a>
+                        <a href="#" onClick={() => onLinkClick('About')}>Photography</a>
                     </div>
                     <div className={`Nav-Item ${activeLink === 'Contact' ? 'active' : ''}`}>
-                        <a href="#" onClick={() => onLinkClick('Contact')}>Contact</a>
+                        <a href="#" onClick={() => onLinkClick('Contact')}>About</a>
                     </div>
                 </div>
             </div>
