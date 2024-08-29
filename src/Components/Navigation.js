04a5013b9fa76b24783c8859ad612f49354eaa12
KrisHHFI <kristopherpepperuk@gmail.com>
2024-08-29 09:35:01 +0300

    Better navigation styling 2

--- src/Components/Navigation.js
+++ src/Components/Navigation.js
@@ -9,7 +9,6 @@ export default function Navigation({ activeLink, onLinkClick }) {
                 <img src={NavigationImageSources.kpBlack} alt="Company Logo" className="Navigation-KPText Hidden-In-Mobile-View" />
                 <img src={NavigationImageSources.kpWhite} alt="Company Logo" className="Mobile-Navigation-Logo Hidden-By-Default" />
             </div>
-            <img src={NavigationImageSources.menuIcon} alt="Menu icon" className="Menu-Icon" />
             <div className="Navigation-Child Navigation-Child-Hidden">
                 <img src={NavigationImageSources.portfolioText} alt="Company Text" className="Navigation-PortfolioText" />
             </div>
@@ -28,6 +27,22 @@ export default function Navigation({ activeLink, onLinkClick }) {
             </div>
             <div className="Navigation-Social-Icon-Container">
                 <SocialIcons />
+            </div>
+
+            <img src={NavigationImageSources.menuIcon} alt="Menu icon" className="Menu-Icon" />
+
+            <div className="Mobile-Nav-Menu Hidden-By-Default">
+                <div className="Mobile-Nav-Links-Container">
+                    <div className={`Mobile-Nav-Link`}>
+                        <a href="#" onClick={() => onLinkClick('Coding')}>Coding</a>
+                    </div>
+                    <div className={`Mobile-Nav-Link`}>
+                        <a href="#" onClick={() => onLinkClick('Photography')}>Photos</a>
+                    </div>
+                    <div className={`Mobile-Nav-Link`}>
+                        <a href="#" onClick={() => onLinkClick('About')}>About</a>
+                    </div>
+                </div>
             </div>
         </div>
     );
